185b0a145f2676f94b0d669679fb1da772a2490c
L-Train <Ltrain23!>
2018-12-28 16:52:41 -0500

    update

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,30 @@
-$( document ).ready(function() {
-    
-    
+// $(document).ready(function () {
 
 
-    for(var i = 0; i < 20 ; i++){
+    // for (var i = 0; i < 20; i++) {
 
-        var button = $("<button type='button' class='btn btn-primary'>Primary " + i + "</button>")
+    //     var button = $("<button type='button' class='btn btn-primary'>Primary " + i + "</button>")
 
-        $("#buttons").append(button)
+    //     $("#buttons").append(button)
 
-    }
-});
+    // }
+
+
+    //javascript, jQuery
+    // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=HWCMzdIscoWEmrUmqEBvNMwMlCJaGd4q&limit=5");
+    // xhr.done(function (data) { console.log("success got data", data); });
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=HWCMzdIscoWEmrUmqEBvNMwMlCJaGd4q&limit=5";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log("success got data", response);
+
+    });
+
+// });
+
+
